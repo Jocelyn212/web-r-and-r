79fe5ad74f9dc56714d2733b0c145cf02c594adf
Jocelyn <jocelyn.cf@gmail.com>
2023-11-21 17:17:39 +0100

    conecciones de backend

--- frontend/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as filmografia } from './filmografia';
-export { default as home } from './home';
-export { default as about } from './about';
-export { default as intro } from './intro';
